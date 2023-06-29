c9322f1749fe27d469d49a954529591b868c6bd3
swknjj <sinwook9255@gmail.com>
2023-06-29 17:54:05 +0900

    Security 내용 추가

--- src/main/java/com/example/qpc/config/SecurityConfig.java
+++ src/main/java/com/example/qpc/config/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.example.qpc.config;
 
+import com.example.qpc.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    protected MemberService memberService;
 
 
     // 첫 잠금 해제
@@ -18,11 +27,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // post , put 공격 방어
         http.csrf().disable();
+//        http
+//                .formLogin()
+//                // 로그인 페이지 URL 설정
+//                .loginPage("/member/login")
+//                .defaultSuccessUrl("/")
+//                .usernameParameter("memberId")
+//                .failureUrl("/member/login/error")
+//                .and()
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+//                .logoutSuccessUrl("/");
     }
 
     // 회원가입할때 비밀번호를 암호화함
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
