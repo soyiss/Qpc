d5d64c5011ce2548ffa76d00dc48f23b1354efad
Kongsis <lee_scyj@nate.com>
2023-07-06 19:34:37 +0900

    HomeController index 수정

--- src/main/java/com/example/qpc/controller/HomeController.java
+++ src/main/java/com/example/qpc/controller/HomeController.java
@@ -3,11 +3,12 @@ package com.example.qpc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
-
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
         return "index";
     }
 }
