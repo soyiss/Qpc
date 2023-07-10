ce87ec5b4feb7c546337c230d5608b1bcc7f5437
swknjj <sinwook9255@gmail.com>
2023-07-10 11:05:43 +0900

    아이디를 검색받아서 회원정보 가져오기

--- src/main/java/com/example/qpc/controller/PaymentController.java
+++ src/main/java/com/example/qpc/controller/PaymentController.java
@@ -1,7 +1,9 @@
 package com.example.qpc.controller;
 
 import com.example.qpc.dto.EmailAuthRequestDTO;
+import com.example.qpc.dto.MemberDTO;
 import com.example.qpc.dto.PaymentMemberDTO;
+import com.example.qpc.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,16 +11,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
+import javax.persistence.EntityNotFoundException;
 import java.io.UnsupportedEncodingException;
 
 @Controller
 @RequiredArgsConstructor
 public class PaymentController {
+    private final PaymentService paymentService;
 
     @PostMapping("/payment/findById")
     public ResponseEntity paymentFindById(@RequestBody PaymentMemberDTO paymentMemberDTO) throws MessagingException, UnsupportedEncodingException {
         System.out.println("paymentMemberDTO = " + paymentMemberDTO);
-        return new ResponseEntity<>(paymentMemberDTO, HttpStatus.OK);
+        try {
+            MemberDTO memberDTO = paymentService.paymentFindById(paymentMemberDTO.getKeyword());
+            return new ResponseEntity<>(memberDTO.getId(),HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
 }
