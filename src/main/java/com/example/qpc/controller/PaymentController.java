70382bd543e4f4e9a03458a8d50ed2405d1a4dbd
swknjj <sinwook9255@gmail.com>
2023-07-11 15:31:53 +0900

    결제한 후 시간충전을 위한 메소드 memberCharge

--- src/main/java/com/example/qpc/controller/PaymentController.java
+++ src/main/java/com/example/qpc/controller/PaymentController.java
@@ -2,7 +2,9 @@ package com.example.qpc.controller;
 
 import com.example.qpc.dto.EmailAuthRequestDTO;
 import com.example.qpc.dto.MemberDTO;
+import com.example.qpc.dto.PaymentInformationDTO;
 import com.example.qpc.dto.PaymentMemberDTO;
+import com.example.qpc.service.MemberService;
 import com.example.qpc.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,10 +18,12 @@ import java.io.UnsupportedEncodingException;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/payment")
 public class PaymentController {
     private final PaymentService paymentService;
+    private final MemberService memberService;
 
-    @PostMapping("/payment/findById")
+    @PostMapping("/findById")
     public ResponseEntity paymentFindById(@RequestBody PaymentMemberDTO paymentMemberDTO) throws MessagingException, UnsupportedEncodingException {
         System.out.println("paymentMemberDTO = " + paymentMemberDTO);
         try {
@@ -28,6 +32,17 @@ public class PaymentController {
         } catch (EntityNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
+    }
+
+    @PostMapping("/memberCharge")
+    public ResponseEntity memberCharge(@RequestBody PaymentInformationDTO paymentInformationDTO) {
+        MemberDTO memberDTO = memberService.findById(paymentInformationDTO.getMemberId());
+        System.out.println("memberDTO = " + memberDTO);
+        memberDTO.setOverTime(memberDTO.getOverTime()+paymentInformationDTO.getTime());
+        memberDTO.setTotalTime(memberDTO.getTotalTime()+ paymentInformationDTO.getTime());
+        memberService.memberUpdate(memberDTO);
+
+        return new ResponseEntity<>(memberDTO,HttpStatus.OK);
     }
 
 }
