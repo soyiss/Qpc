0e00bbae3bcfebc1e481e8de8089ea300cbca78b
soyiss <zwcrvl@naver.com>
2023-07-17 09:48:30 +0900

    게임 카테고리 레포지토리

---
+++ src/main/java/com/example/qpc/repository/GameCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.example.qpc.repository;
+
+import com.example.qpc.entity.GameCategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameCategoryRepository extends JpaRepository<GameCategoryEntity,Long> {
+}
