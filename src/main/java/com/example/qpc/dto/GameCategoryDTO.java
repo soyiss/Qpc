6871e637037c6366830847c561b0ea35c05d5b06
soyiss <zwcrvl@naver.com>
2023-07-17 09:47:47 +0900

    게임 카테고리 DTO

---
+++ src/main/java/com/example/qpc/dto/GameCategoryDTO.java
@@ -0,0 +1,23 @@
+package com.example.qpc.dto;
+
+import com.example.qpc.entity.GameCategoryEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameCategoryDTO {
+
+    private Long id;
+    private String gameCategoryName;
+
+    public static GameCategoryDTO toDTO(GameCategoryEntity gameCategoryEntity) {
+        GameCategoryDTO gameCategoryDTO = new GameCategoryDTO();
+        gameCategoryDTO.setId(gameCategoryEntity.getId());
+        gameCategoryDTO.setGameCategoryName(gameCategoryEntity.getGameCategoryName());
+        return gameCategoryDTO;
+    }
+
+}
